d96d18a2142d8ac3d3bf7de729f00cd266124f82
AlphaWolf21 <abusedmaster71@gmail.com>
2016-10-05 09:03:16 -0400

    Create GuiHandler.java

---
+++ src/main/java/killbait/PrimordialCrops/Utils/GuiHandler.java
@@ -0,0 +1,14 @@
+package killbait.PrimordialCrops.Utils;
+
+public class GuiHandler implements IGuiHandler {
+
+    @Override
+    public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
+        return null;
+    }
+
+    @Override
+    public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
+        return null;
+    }
+}
