fbbd9ab8eb3b4acad330d2e66a3b78f1cebe8b1e
AlphaWolf21 <abusedmaster71@gmail.com>
2016-10-11 09:21:29 -0400

    Update GuiBook.java

--- src/main/java/killbait/PrimordialCrops/Utils/GuiBook.java
+++ src/main/java/killbait/PrimordialCrops/Utils/GuiBook.java
@@ -1,11 +1,25 @@
 package killbait.PrimordialCrops.Utils;
 
+//Have not tested if this will work or not, Writting the code directly into github until i clone the source, Imports are needed
 public class GuiBook extends GuiScreen {
-
+    
+    	public static final ResourceLocation texture = new ResourceLocation("maingui.png");
+    
+    int xSize, ySize, left, top;
+    
+    
 @Override
 public void drawScreen(int mouseX, int mouseY, float partialTicks) {
-    this.drawDefaultBackground();
-    super.drawScreen(mouseX, mouseY, partialTicks);
+    drawDefaultBackground();
+
+		GlStateManager.color(1F, 1F, 1F);
+		mc.getTextureManager().bindTexture(texture);
+		drawTexturedModalRect(left, top, 0, 0, xSize, ySize);
+
+		super.drawScreen(mouseX, mouseY, partialTicks);
+
+			String betaTest = I18n.translateToLocal("Primordial Crops");
+			mc.fontRendererObj.drawStringWithShadow(betaTest, left + xSize / 2 - mc.fontRendererObj.getStringWidth(betaTest) / 2, top - 12, 0xFFFFFF);
 }
 
 @Override
@@ -13,6 +27,13 @@ public boolean doesGuiPauseGame() {
     return false;
 }
 
+@Override
+	public void initGui() {
+		xSize = 256;
+		ySize = 184;
+		left = (width - xSize) / 2;
+		top = (height - ySize) / 2;
 
+	}
 
 }
